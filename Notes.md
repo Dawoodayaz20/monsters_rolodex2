b6617a5f8a0a02c3b36ccb7e79ac6b014f380b27
Dawood Ayaz <dawoodayaz18@gmail.com>
2025-02-18 23:20:20 +0500

    Making first REACT App.

---
+++ Notes.md
@@ -0,0 +1,7 @@
+##Notes from [Udemy] "REACT Course" : 
+[REACT-App] uses [Babel] and [Webpack].
+[Babel] : Babel is a tool that makes sure that all your JavaScript works in all the browsers with different environments.
+[Webpack] : Webpack takes all our JavaScript files and bundle everything up and optimize them for production.
+So [REACT-App] does all of this for us in the backend with a team of developers working on it. 
+[map] function in [REACT] returns us the value of a function iterated over all the items of an array. 
+[REACT] needs a unique [key] for items in an array so that when the name or any other attribute gets updated in that array, 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.components';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// class App extends Component {
+//   constructor(){
+//     super();
+//     this.state = {
+//       string: 'Hello Sir! This is TARS.'
+//     };
+//   }
 
+//   render(){
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <p>{this.state.string}</p>
+//           <button onClick={() => this.setState({ string : 'Hello from Interstellar Bot'})}>Learn React</button>
+//         </header>
+//       </div>
+//   );
+// }
+// }
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: []
+    }
+  }
+
+    componentDidMount(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}));
+    }
+
+  render(){
+    return (
+      <div className="App">
+        <CardList monsters = {this.state.monsters} /> {/* Anything we pass inside CardList element is a prop and will pass as a parameter(prop) into CardList element*/}    
+      </div>
+    );
+  }
+}
 export default App;
