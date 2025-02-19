f9d99b9e929dd357126918907cf9ce1c67ed1a04
Dawood Ayaz <dawoodayaz18@gmail.com>
2025-02-19 06:35:30 +0500

    Created monsters card and also a searchFilter.

--- src/App.js
+++ src/App.js
@@ -31,7 +31,8 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -42,9 +43,19 @@ class App extends Component {
     }
 
   render(){
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+
     return (
       <div className="App">
-        <CardList monsters = {this.state.monsters} /> {/* Anything we pass inside CardList element is a prop and will pass as a parameter(prop) into CardList element*/}    
+        <input 
+          type='search' 
+          placeholder='search monsters'
+          onChange={e => this.setState({searchField: e.target.value})}/>
+        <CardList monsters = {filteredMonsters} /> {/* Anything we pass inside CardList element is a prop and will pass as a parameter(prop) into CardList element*/}    
       </div>
     );
   }
